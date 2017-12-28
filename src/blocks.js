e0ba54d03571e61819a1fd6a037676e9fc6b1fdf
Eli Kosminsky <ekosminsky@concord.org>
2017-12-28 14:16:55 -0800

    More blocks

---
+++ src/blocks.js
@@ -0,0 +1,79 @@
+import Blockly from 'node-blockly/browser';
+
+export function configureBlocks() {
+  Blockly.Blocks['get_experiment_var'] = {
+    init: function() {
+      this.jsonInit({
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_dropdown",
+            "name": "VAR",
+            "options": [["hour", "time"], ["plants", "plantsNumber"], ["snails", "snailsNumber"]]
+          }
+        ],
+        "output": null,
+        "colour": "%{BKY_VARIABLES_HUE}",
+        "tooltip": "%{BKY_VARIABLES_GET_TOOLTIP}",
+      })
+    }
+  };
+
+  Blockly.JavaScript['get_experiment_var'] = function(block) {
+    // Variable getter.
+    var varName = block.getFieldValue('VAR');
+    return ['this.state.' + varName, Blockly.JavaScript.ORDER_ATOMIC];
+  };
+
+
+  Blockly.Blocks['set_experiment_var'] = {
+    init: function() {
+      this.jsonInit({
+        "message0": "%{BKY_VARIABLES_SET}",
+        "args0": [
+          {
+            "type": "field_dropdown",
+            "name": "VAR",
+            "options": [["hour", "time"], ["plants", "plantsNumber"], ["snails", "snailsNumber"]]
+          },
+          {
+            "type": "input_value",
+            "name": "VALUE"
+          }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "%{BKY_VARIABLES_HUE}",
+        "tooltip": "%{BKY_VARIABLES_SET_TOOLTIP}",
+      })
+    }
+  };
+
+  Blockly.JavaScript['set_experiment_var'] = function(block) {
+    // Variable setter.
+    var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+        Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+    var varName = block.getFieldValue('VAR');
+    return 'yield this.setState({' + varName + ': ' + argument0 + '});\n'
+  };
+
+  Blockly.Blocks['wait'] = {
+    init: function() {
+      this.jsonInit({
+        "message0": "Wait 1 hr",
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "%{BKY_VARIABLES_HUE}",
+        "tooltip": "%{BKY_VARIABLES_SET_TOOLTIP}",
+      })
+    }
+  };
+
+  Blockly.JavaScript['wait'] = function(block) {
+    // Variable setter.
+    var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+        Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+    var varName = block.getFieldValue('VAR');
+    return 'this.setState({' + varName + ': ' + argument0 + '});\n'
+  };
+}
--- src/main.js
+++ src/main.js
@@ -2,38 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Blockly from 'node-blockly/browser';
 import Application from './application';
+
+import { configureBlocks } from './blocks';
  
 document.addEventListener('DOMContentLoaded', function() {
-  Blockly.Blocks['set_experiment_var'] = {
-    init: function() {
-      this.jsonInit({
-        "message0": "%{BKY_VARIABLES_SET}",
-        "args0": [
-          {
-            "type": "field_dropdown",
-            "name": "VAR",
-            "options": [["hour", "time"]]
-          },
-          {
-            "type": "input_value",
-            "name": "VALUE"
-          }
-        ],
-        "previousStatement": null,
-        "nextStatement": null,
-        "colour": "%{BKY_VARIABLES_HUE}",
-        "tooltip": "%{BKY_VARIABLES_SET_TOOLTIP}",
-      })
-    }
-  };
-
-  Blockly.JavaScript['set_experiment_var'] = function(block) {
-    // Variable setter.
-    var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
-        Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-    var varName = block.getFieldValue('VAR');
-    return 'this.setState({' + varName + ': ' + argument0 + '});\n'
-  };
+  configureBlocks()
 
   ReactDOM.render(
     React.createElement(Application),
